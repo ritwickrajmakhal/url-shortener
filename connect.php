35be4e483f16ed7d399a4c1bd6c3feabee8c9277
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-08-07 22:29:00 +0530

    fixes: Uncaught Dotenv\Exception\InvalidPathException: Unable to read any of the environment file(s) at [/home/site/wwwroot/.env]. in /home/site/wwwroot/vendor/vlucas/phpdotenv/src/Store/FileStore.php:68

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 define('DATABASE_HOST', $_ENV['DATABASE_HOST']);
